7327d40d77f81dc9ffd6b9ab27f5d9e5f38f11dc
srikanthmudili <srikanth512000@gmail.com>
2020-05-07 13:55:21 +0530

    added admin form

--- src/Admin.js
+++ src/Admin.js
@@ -1,54 +1,83 @@
-import React from 'react';
-import {Form,Dropdown,DropdownButton} from 'react-bootstrap'
-const Admin = (props) => {
+import React,{useState} from 'react';
+import {Form} from 'react-bootstrap'
+import './Admin.css'
+const Admin = () => {
+    const [contest,setContest]=useState('');
+    const [description,setDescription]=useState('');
+    const [cost,setCost]=useState(0);
+    const [date,setDate]=useState('');
+    const [time,setTime]=useState('');
+    const [participants,setParticipants]=useState(0);
+    const [winners,setWinners]=useState(0);
+    const [prize,setPrize]=useState([]);
+    const l=[];
+    const unixTime=new Date(date+','+time).getTime() / 1000;
+    for(let i=0;i<winners;i++)
+          l.push(i);
+    
     const post=(e)=>{
         e.preventDefault();
-        console.log(props.contest);
+        console.log(date,time,prize);
+        cancelCourse();
+        setPrize([]);
     }
+    const cancelCourse = () => { 
+      document.getElementById("comp").reset();
+    }
+    
+
     return (
         <div>
-            <Form className='col' onSubmit={post} >
+           <h1>Time is {unixTime}{postDate()}</h1>
+            <Form className='col' id='comp' onSubmit={post} >
              <Form.Group>
-                <Form.Label>Contest Name</Form.Label>
-                <Form.Control  type="text" placeholder="Contest Name" value={props.contest} />
+                <Form.Label className="text">Contest Name</Form.Label>
+                <Form.Control  type="text" placeholder="Contest Name" value={contest} onChange={e=>setContest(e.target.value)} />
              </Form.Group>
              <Form.Group>
-                <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Description" />
+                <Form.Label className="text">Description</Form.Label>
+                <Form.Control type="text" placeholder="Description" value={description} onChange={e=>setDescription(e.target.value)} />
              </Form.Group>
              <Form.Group>
-                <Form.Label>Cost</Form.Label>
-                <Form.Control type="text" placeholder="Cost" />
+                <Form.Label className="text">Cost</Form.Label>
+                <Form.Control type="text" placeholder="Cost" value={cost} onChange={e=>setCost(e.target.value)} />
              </Form.Group>
              <Form.Group>
-                <Form.Label>Last date</Form.Label>
-                <Form.Control type="date"/>
+                <Form.Label className="text">Last date</Form.Label>
+                <Form.Control type="date" value={date} onChange={e=>setDate(e.target.value)}/>
              </Form.Group>
              <Form.Group>
-                <Form.Label>Up Time</Form.Label>
-                <Form.Control type="time"/>
-             </Form.Group>
-             <br/>
-             <DropdownButton variant='info' title="No of Participants ">
-                    <Dropdown.Item>1</Dropdown.Item>
-                    <Dropdown.Item>2</Dropdown.Item>
-                    <Dropdown.Item>3</Dropdown.Item>
-                    <Dropdown.Item>4</Dropdown.Item>
-                    <Dropdown.Item>5</Dropdown.Item>
-             </DropdownButton>
-             <br/>
-             <Form.Group>
-                <Form.Label>prize money</Form.Label>
-                <Form.Control type="time"/>
+                <Form.Label className="text">Up Time</Form.Label>
+                <Form.Control type="time" value={time} onChange={e=>setTime(e.target.value)}/>
              </Form.Group>
              <Form.Group>
-                <Form.Label>Up Time</Form.Label>
-                <Form.Control type="time"/>
+                <Form.Label className="text">Total Participants</Form.Label>
+                <Form.Control type="number" placeholder='count' value={participants} onChange={e=>setParticipants(e.target.value)} />
              </Form.Group>
-             <button type="submit" className="btn btn-primary">Submit</button>
+             <Form.Group>
+             <select type="number"className='btn btn-info'id='button' value={winners} onChange={e=>setWinners(e.target.value)}>
+                <option value=''>No of Winners</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+             </select>
+             </Form.Group>
+             {l.map(i=> <Form.Group key={i}>
+                                <Form.Label className="text">Prize {i+1}</Form.Label>
+                                <Form.Control type="input" placeholder='count' value={prize[i]} onInput={prize.splice(i,0,InputEvent)} />
+                        </Form.Group>,)}
+             <button id='button' type="submit" className="btn btn-primary">Submit</button>
             </Form>
         </div>
     );
 };
-
+const postDate=(separator = '-')=>{
+   let date = new Date();
+   let d = date.getDate();
+   let m = date.getMonth()+1;
+   let y = date.getFullYear();
+   return(d+separator+m+separator+y)
+ }
 export default Admin;
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+/*<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.14.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAnoLXmI5cDFpsByz4VFrfPPosLoxSHUYE",
+    authDomain: "talent-connect-98cc1.firebaseapp.com",
+    databaseURL: "https://talent-connect-98cc1.firebaseio.com",
+    projectId: "talent-connect-98cc1",
+    storageBucket: "talent-connect-98cc1.appspot.com",
+    messagingSenderId: "165714324900",
+    appId: "1:165714324900:web:cd91d22c99430af8df6531",
+    measurementId: "G-HV6EQ86RQR"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>)}*/
